16feb49f807611e2f456080df1c507c25776dc96
sanixto <sanix.to@gmail.com>
2023-03-18 15:56:29 +0200

    add handler for errors

---
+++ src/handlerErrors.js
@@ -0,0 +1,8 @@
+const handlerErrors = new Map([
+  [400, '400 Bad Request'],
+  [401, '401 Unauthorized access'],
+  [404, '404 Requested resource not found'],
+  [500, '500 Internal server error'],
+]);
+
+export default handlerErrors;
