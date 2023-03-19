89de71881b553e42120bbbff3c36c77f2f78fb03
sanixto <sanix.to@gmail.com>
2023-03-19 23:32:25 +0200

    add animation for dowloading video

--- src/Components/CourseDetailPage/VideoPlayer.js
+++ src/Components/CourseDetailPage/VideoPlayer.js
@@ -1,4 +1,5 @@
 import Hls from 'hls.js';
+import Spinner from 'react-bootstrap/Spinner';
 
 function VideoPlayer({ urlVideo, urlPoster }) {
   const video = document.getElementById('video');
@@ -8,14 +9,43 @@ function VideoPlayer({ urlVideo, urlPoster }) {
 
   const handlePlay = (e) => {
     const curVideo = e.target;
+    if (!curVideo.readyState) return;
     if (curVideo.paused) curVideo.play();
     else curVideo.pause();
+    // alert(curVideo.readyState)
+  };
+
+  const handleWaiting = () => {
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = 'block';
+  };
+
+  const handlePlaying = () => {
+    const spinner = document.getElementById('spinner');
+    spinner.style.display = 'none';
   };
 
   return (
-    <video id="video" onClick={handlePlay} width="100%" poster={urlPoster}>
-      <track kind="captions" />
-    </video>
+    <>
+      <video
+        id="video"
+        onClick={handlePlay}
+        width="100%"
+        poster={urlPoster}
+        onWaiting={handleWaiting}
+        onPlaying={handlePlaying}
+      >
+        <track kind="captions" />
+      </video>
+      <Spinner
+        id="spinner"
+        animation="border"
+        role="status"
+        className="position-relative start-50 bottom-50"
+      >
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </>
   );
 }
 
